0a079d4966cb5055f4856115a4739a9bef9651d2
Diobergue viana <104652732+dioberg@users.noreply.github.com>
2024-08-29 14:18:42 +0000

    desafio 1 e 2

---
+++ concat_dados.py
@@ -0,0 +1,10 @@
+#vamo receber dois dados diferentes do usuario e concatena-los unica strings.
+
+info1 = input("digite o primeiro numero: ")
+info2 = input("digite o segundo numero: ")
+
+
+dados_concatenados = info1 + " " + info2
+
+
+print("dados concatenados: ", dados_concatenados)
---
+++ repet_txt.py
@@ -0,0 +1,9 @@
+
+#vamo solicitar uma string e um numero inteiro como  entrada. depois teremos que retornar a string repetida o numero de vezes informado.
+
+num_string = input("digite uma string: ")
+numero = int(input("digite um numero inteiro: "))
+
+result = (num_string + " ") * numero
+
+print("resultado ", result)
